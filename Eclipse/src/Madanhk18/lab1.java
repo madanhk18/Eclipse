4ec419fd2145529ef432c699229ce0057d2f1edc
madanhk18 <madanhk18@gmail.com>
2024-02-07 21:20:43 +0530

    first lab prog

---
+++ Eclipse/src/Madanhk18/lab1.java
@@ -0,0 +1,37 @@
+package Madanhk18;
+class employee
+{
+	private String name;
+	private int id;
+	private double salary;
+	
+	employee(String name,int ID,double salary)
+	{
+		this.name=name;
+		this.id=ID;
+		this.salary=salary;
+	}
+	void printdata()
+	{
+		System.out.println("Name="+name);
+		System.out.println("ID="+id);
+		System.out.println("salary="+salary);
+		
+	}
+	void hike(double percent)
+	{
+		salary=salary+(salary*percent)/100.0;
+	}
+}
+public class lab1 {
+	public static void main(String args[])
+	{
+		employee e=new employee("Madan",75,10000);
+		System.out.println("Before hike");
+		e.printdata();
+		
+		e.hike(5);
+		System.out.println("AFter hike");
+		e.printdata();
+	}
+}
