fcd06c450a0e1d6a0789991a4d82ff6d64469ffb
madanhk18 <madanhk18@gmail.com>
2024-02-09 23:21:41 +0530

    age program

---
+++ Eclipse/src/Madanhk18/feb9.java
@@ -0,0 +1,20 @@
+package Madanhk18;
+class age
+{
+	
+	void age(int ag)
+	{
+		System.out.println("Age="+ag);
+	}
+
+}
+
+public class feb9 {
+
+	public static void main(String[] args) {
+		age a=new age();
+	a.age(10);
+
+	}
+
+}
